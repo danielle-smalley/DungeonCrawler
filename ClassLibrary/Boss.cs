0d8e9eee862675501d7c7d228bdd956a2f801253
Danielle Smalley <smalley.danielle@gmail.com>
2021-06-04 20:40:06 -0500

    Initial Commit

---
+++ ClassLibrary/Boss.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Boss : Character
+    {
+        //fields
+
+        //props
+
+        //ctors
+
+        //methods
+
+
+    }//end class
+}//end namespace
