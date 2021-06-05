b093b1d0d9875fdc49484e148f93f22121c072a6
Danielle Smalley <smalley.danielle@gmail.com>
2021-06-04 20:05:36 -0500

    Initial commit

---
+++ ClassLibrary/Dancer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public sealed class Dancer : Character //Dancer is child of Character class. Inherits fields, props, and methods. Sealed means I'm not  using this class as a base class for other classes, it's sealed off.
+    {
+        //fields - N/A
+        //props
+        //public Persona DancerPersona
+
+        //ctors
+
+        //methods
+
+    }//end class
+}//end namespace
